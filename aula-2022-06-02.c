155bfc14f63742b223b91c9918d1dac8ff38230b
luiz-devjr <106721714+luiz-devjr@users.noreply.github.com>
2022-06-03 18:45:27 -0300

    Update aula-2022-06-02.c

--- aula-2022-06-02.c
+++ aula-2022-06-02.c
@@ -1,1 +1,44 @@
+//2021201406
+  /*
+  entrada: valores recebidos da semana
+  saida: gastos da semana
+  
+  inicio:
+   ler primeira entrada de segunda e receber a saida de segunda S
+   calcular o caixa do dia entrada-saida S1
+   resultado= entrada-saida
+   
+   ler primeira entrada de terca e receber a saida de terca T
+   calcular o caixa do dia entrada-saida T1
+   resultado= entrada-saida
+   
+   ler primeira entrada de quarta e receber a saida de quarta Q
+   calcular o caixa do dia entrada-saida Q1
+   resultado= entrada-saida
+   
+   ler primeira entrada de quinta e receber a saida de quinta QQ
+   calcular o caixa do dia entrada-saida QQ1
+   resultado= entrada-saida
+   
+   ler primeira entrada de sexta e receber a saida de sexta SS
+   calcular o caixa do dia entrada-saida SS1
+   resultado= entrada-saida
+  
+  calcular o valor entrada da semana
+  resultado = S+T+Q+QQ+SS
+  calcular o valor de saida da semana
+  resultado = S1+T1+Q1+QQ1+SS1
+  
+  se o valor de entrada-saida for > 2000 e <500
+  entao
+   "mostrar o valor de entrada total da semana
+	mostrar o  valor de saida total da semana 
+	mostrar o saldo positivo do resultado de entrada-saida da semana"
+	
+  senao
+   "mostrar o valor de entrada total da semana
+	mostrar o  valor de saida total da semana 
+	mostrar o saldo negativo do resultado"	
+  
 
+fim*/
