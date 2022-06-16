ab38820c9412d537df1c74c10028c0c04d4019fb
luiz-devjr <106721714+luiz-devjr@users.noreply.github.com>
2022-06-15 22:37:48 -0300

    Create exerc_cap2_7.c

---
+++ exerc_cap2_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+ int main()
+{
+  float numero;
+  
+  printf("digite um numero: \n");
+  scanf ("%f",&numero);
+  
+ if (numero <=50 && numero>=20)
+  printf ("esta entre 20 e 50");
+ 
+ else 
+  printf("nao esta entre 20 e 50");
+  
+}
