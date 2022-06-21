677a29ad438a80d54668b2942f964f8620ea256c
luiz-devjr <106721714+luiz-devjr@users.noreply.github.com>
2022-06-21 20:36:54 -0300

    Update exerc_cap2_16.c

--- exerc_cap2_16.c
+++ exerc_cap2_16.c
@@ -31,7 +31,46 @@ int main ()
   	
 }
 
+//programa com switch case
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int idade;
+	
+	printf("informe a idade do competidor: \n");
+	scanf("%d",&idade);
+	 
+ /*do {
+  printf("idade invalida:  \n");
+  scanf ("%d", &idade);
+}while (idade <= 5); esta dando erro,tem que executar duas vezes para funcionar*/
+
+ switch (idade)
+ {
+  case 5 ... 7:
+   printf ("categoria infantil A");
+  break;
+  
+  case 8 ... 10:
+   printf ("categoria infantil B");
+  break;
+  
+  case 11 ... 13:
+   printf ("categoria juvenil A");
+  break;
+  
+  case 14 ... 17:
+   printf ("categoria juvenil B");
+  break;
+  
+   default:
+   printf ("Senior");
+ }
+ 
+}
 
 
 
