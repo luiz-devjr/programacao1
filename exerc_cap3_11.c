fc4da535012c9036e001d21311beab37d30a47dc
luiz-devjr <106721714+luiz-devjr@users.noreply.github.com>
2022-06-22 23:54:54 -0300

    Create exerc_cap3_11.c

---
+++ exerc_cap3_11.c
@@ -0,0 +1,21 @@
+//nao terminei,por que n entendi ao certo o que o problema quer!!!
+#include <stdio.h>
+
+
+int main()
+{
+	int numeros;
+	
+	printf("digite um numero:");
+	scanf ("%d", &numeros);
+	
+	
+	do
+  {
+    printf("digite um numero:");
+	scanf ("%d", &numeros);
+  	
+  }while(numero=0);
+	
+	
+}
