a086354efbd7cc6f51221b1f6ff1515728cea3ba
luiz-devjr <106721714+luiz-devjr@users.noreply.github.com>
2022-06-28 23:21:16 -0300

    Update exerc_cap2_5.c

--- exerc_cap2_5.c
+++ exerc_cap2_5.c
@@ -12,8 +12,12 @@
   printf("resultado: %d\n",divisao);
 
  if (a% 3==0)
-  printf("\n o numero eh divisivel por 3\n");
+  printf("o numero eh divisivel por 3\n");
  else 
- printf ("\nnao eh divisivel por 3\n");
+ printf ("nao eh divisivel por 3\n");
 
+ if (a% 7==0)
+  printf("o numero eh divisivel por 7\n");
+ else 
+ printf ("nao eh divisivel por 7\n");
 }
